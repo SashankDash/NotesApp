cb41967a566e738562b1e208096fe403b66f2477
Sashank <sashanksekharbapun@gmail.com>
2020-08-04 12:24:32 +0530

    1st

---
+++ app.js
@@ -0,0 +1,13 @@
+// const add = require('./utils.js')
+
+// const sum = add(7,9)
+
+// console.log(add(3,2))
+
+// console.log(sum)
+
+const getNotes = require('./notes.js')
+
+const letter = getNotes()
+
+console.log(letter)
